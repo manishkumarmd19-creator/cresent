865aa9c9c785ecab46a39fd332376f87b22effd1
manishkumarmd19-creator <manishkumarmd19@gmail.com>
2026-09-23 14:50:27 +0530

    Vintage Italian pizzeria typography redesign - red script on cream

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Lobster, Montserrat, Great_Vibes } from "next/font/google";
+import { Yellowtail, Montserrat, Great_Vibes } from "next/font/google";
 import "./globals.css";
 
-const lobster = Lobster({
-  variable: "--font-lobster",
+const heading = Yellowtail({
+  variable: "--font-heading",
   weight: "400",
   subsets: ["latin"],
 });
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${lobster.variable} ${greatVibes.variable} ${montserrat.variable} h-full antialiased`}
+      className={`${heading.variable} ${greatVibes.variable} ${montserrat.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
